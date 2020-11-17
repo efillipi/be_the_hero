78f16568482b224d28617b14468bfa4dcb9628d1
Edney Fillipi <61793102+efillipi@users.noreply.github.com>
2020-11-17 11:29:16 -0300

    front-end

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './global.css';
+
+import Logon from './Pages/Logon';
+import Routes from './routes';
+
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
