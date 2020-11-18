b537c22f5c30b87f207f7d05b02400991ce8c088
Edney Fillipi <61793102+efillipi@users.noreply.github.com>
2020-11-18 09:01:29 -0300

    comimit react + back end

--- frontend/src/Pages/Register/index.js
+++ frontend/src/Pages/Register/index.js
@@ -2,12 +2,33 @@ import React, { useState } from 'react'
 import { Link, useHistory } from 'react-router-dom'
 import { FiArrowLeft } from 'react-icons/fi'
 
-
+import api from '../../services/api';
 import './styles.css'
 
 import logoImg from '../../assets/logo.svg'
 
 export default function Register() {
+
+  const [name, setname] = useState("");
+  const [email, setemail] = useState("");
+  const [WhatsApp, setWhatsapp] = useState("");
+  const [city, setcity] = useState("");
+  const [uf, setuf] = useState("");
+
+  const data= {
+    name,
+    email,
+    WhatsApp,
+    city,
+    uf,
+  };
+
+  console.log(data);
+
+
+  function handleRegister(e){
+    e.preventDefault();
+  }
   
   return (
     <div className="register-container">
@@ -21,13 +42,37 @@ export default function Register() {
             Não tenho cadastro
           </Link>
         </section>
-        <form >
-          <input placeholder="Nome da ONG"   />
-          <input type="email" placeholder="Email"  />
-          <input placeholder="WhatsApp" />
+        <form onSubmit={handleRegister} >
+          <input 
+            placeholder="Nome da ONG"
+            value= {name} 
+            onChange={e => setname(e.target.value)}
+          />
+          <input type="email" 
+            placeholder="Email"  
+            value= {email} 
+            onChange={e => setemail(e.target.value)}
+          />
+          
+          <input 
+            placeholder="WhatsApp" 
+            value= {WhatsApp} 
+            onChange={e => setWhatsapp(e.target.value)}
+          />
+          
           <div className="input-group">
-            <input placeholder="Cidade"  />
-            <input placeholder="UF" />
+            <input 
+              placeholder="Cidade"  
+              value= {city} 
+              onChange={e => setcity(e.target.value)}
+            />
+            
+            <input 
+              placeholder="UF"  style={{ width :80 }}
+              value= {uf} 
+              onChange={e => setuf(e.target.value)}
+            />
+            
           </div>
           <button className="button" type="submit"> Cadastrar </button>
         </form>
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,6 +3,10 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 import Logon from './Pages/Logon';
 import Register from './Pages/Register';
+import Profile from './Pages/Profile';
+import NewIncident from './Pages/NewIncident';
+
+
 
 export default function Routes(){
     
@@ -11,6 +15,10 @@ export default function Routes(){
     <Switch>
         <Route path="/" exact component={Logon}/>  
         <Route path="/register" component={Register}/>
+        <Route path="/profile" component={Profile}/>
+        <Route path="/incidents/new" component={NewIncident}/>
+
+
     </Switch>
    </BrowserRouter>
    );
